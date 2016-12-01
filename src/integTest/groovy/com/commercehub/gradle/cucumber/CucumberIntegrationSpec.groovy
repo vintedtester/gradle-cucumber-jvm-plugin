3f306c2b17a8736ab6f3fa58593b65e2765d2039
balajilinks <balajilinks@gmail.com>
2016-12-01 11:00:02 +0000

    File paths changed to cope reporting update

--- src/integTest/groovy/com/commercehub/gradle/cucumber/CucumberIntegrationSpec.groovy
+++ src/integTest/groovy/com/commercehub/gradle/cucumber/CucumberIntegrationSpec.groovy
@@ -46,7 +46,7 @@ class CucumberIntegrationSpec extends IntegrationSpec {
         !result.wasUpToDate(':compileTestGroovy')
         result.wasExecuted(':test')
         new File(projectDir, 'build/reports/test').list().flatten()*.replace('\\', '/')
-            .findResults { it.matches('features-happypath-feature_(feature)?\\.html') ? it : null } != null
+            .findResults { it.matches('report-feature_features-happypath-feature?\\.html') ? it : null } != null
     }
 
     def testFailingBackgroundStep() {
